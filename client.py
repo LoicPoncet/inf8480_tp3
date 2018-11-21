51bedd39595a5dd753c2cbb8e5dcb513da65b760
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-11-21 18:23:13 -0500

    Client to make requests

--- client.py
+++ client.py
@@ -1,21 +1,44 @@
-import os
 import sys
 import time
 import asyncio
+import aiohttp
 
-def get(url):
-    os.system('wget ' + url)
+ROUNDS = 5
+REQUESTS = 30
 
-def main():
+async def make_request(session, url):
+    """
+    Makes a requuets to the given url.
+    """
+    async with session.get(url) as response:
+        return response
+
+
+async def batch(session):
+    """
+    Runs a batch of REQUESTS number of requests.
+    """
+    await asyncio.wait([make_request(session, 'http://' + sys.argv[1] + ':8080?nom=perfTest') for _ in range(REQUESTS)])
+
+async def main():
     if len(sys.argv) != 2:
         print("Usage: \"python client.py IP_ADDR\"")
-    else:
-        total_time = 0
-        for _ in range(30):
+        sys.exit(1)
+    average = 0
+    async with aiohttp.ClientSession() as session:
+        for i in range(ROUNDS):
+            print("Running batch {} on {} with {} requests..".format(i+1, ROUNDS, REQUESTS))
             start_time = time.time()
-            get(sys.argv[1] + ":8080/?nom=Loic")
-            elapsed_time = time.time() - start_time
-            total_time += elapsed_time
-        print(total_time / 30)
+            await batch(session)
+            elapsed = time.time() - start_time
+            print("Elapsed time for batch {}: {}".format(i+1, elapsed))
+            average += elapsed
+    average /= ROUNDS
+    print("Average time for {} requests on {} batches: {}".format(
+        REQUESTS,
+        ROUNDS,
+        average
+    ))
 
-main()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
