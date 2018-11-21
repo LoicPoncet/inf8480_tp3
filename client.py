c141b9e7c138b7ffac431b970ac639e120775552
Loic Poncet <loicponcet05@gmail.com>
2018-11-21 11:22:06 -0500

    LoadBalancing seems to be working

--- client.py
+++ client.py
@@ -1,12 +1,10 @@
 import os
 import sys
 import time
+import asyncio
 
 def get(url):
-    start_time = time.time()
     os.system('wget ' + url)
-    elapsed_time = time.time() - start_time
-    return elapsed_time
 
 def main():
     if len(sys.argv) != 2:
@@ -14,7 +12,10 @@ def main():
     else:
         total_time = 0
         for _ in range(30):
-            total_time += get(sys.argv[1] + ":8080/?nom=Loic")
+            start_time = time.time()
+            get(sys.argv[1] + ":8080/?nom=Loic")
+            elapsed_time = time.time() - start_time
+            total_time += elapsed_time
         print(total_time / 30)
 
 main()
